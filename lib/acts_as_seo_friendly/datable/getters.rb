8d960ed28b92ad7288f737b42ad982f53d4d029f
Timur Khafizov <timur.khafizov@fishtag.ru>
2015-03-30 17:13:45 +0300

    Update getters to check if seo datum presents in any way and is persisted

--- lib/acts_as_seo_friendly/datable/getters.rb
+++ lib/acts_as_seo_friendly/datable/getters.rb
@@ -3,7 +3,7 @@ module ActsAsSeoFriendly
     module Getters
       ActsAsSeoFriendly::Datable::Core::SEO_ATTRIBUTES.each do |seo_attr|
         define_method(seo_attr) do
-          if seo_datum.persisted?
+          if seo_datum && seo_datum.persisted?
             seo_datum.public_send(seo_attr)
           else
             public_send("dynamic_#{seo_attr}")
